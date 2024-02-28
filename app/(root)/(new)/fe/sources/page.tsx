ec0054ff44750b340709530c686f5b8142ed57ce
Ritul Bhatnagar <ritulbhatnagar15@gmail.com>
2024-02-29 01:33:02 +0530

    changes2

--- app/(root)/(new)/fe/sources/page.tsx
+++ app/(root)/(new)/fe/sources/page.tsx
@@ -19,22 +19,22 @@ const Sources = () => {
 <div className="">
   <div className="flex flex-wrap mx-2">
     {sources.map((source, index) => (
-      <div key={source._id} className= "w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-2 mb-4">
-        <Link   href={`/source/${source._id}`}>
+      <div key={source?._id} className= "w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-2 mb-4">
+        <Link   href={`/source/${source?._id}`}>
 
         <Card className="py-4 my-6 mx-5 bg-gray-100 cursor-pointer">
          
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
               <p className="text-tiny uppercase font-bold">Articles</p>
               <small className="text-default-900">{source?.count} articles</small>
-              <h3 className="font-bold text-large">{source._id}</h3>
+              <h3 className="font-bold text-large">{source?._id}</h3>
             </CardHeader>
           
           <CardBody className="overflow-visible py-2">
             <Image
               alt="Card background"
               className="object-cover rounded-xl"
-              src={`/logos/${source._id}.svg`}
+              src={`/logos/${source?._id}.svg`}
               width={100}
               />
           </CardBody>
